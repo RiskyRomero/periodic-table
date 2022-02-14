0c043aa6d83cab4b705857d580d09f4d85b73e5c
Ryumin <44841166+RiskyRomero@users.noreply.github.com>
2022-02-15 03:57:55 +0900

    chore: element - cont

---
+++ dict.py
@@ -0,0 +1,110 @@
+periodic_elements = [
+    {
+        "atomic_num": 1,
+        "chem_sym": "H",
+        "chem_name": "Hydrogen",
+        "atom_weight": 1.008
+    },
+    {
+        "atomic_num": 2,
+        "chem_sym": "He",
+        "chem_name": "Helium",
+        "atom_weight": 4.003
+    },
+    {
+        "atomic_num": 3,
+        "chem_sym": "Li",
+        "chem_name": "Lithium",
+        "atom_weight": 6.941
+    },
+    {
+        "atomic_num": 4,
+        "chem_sym": "Be",
+        "chem_name": "Beryllium",
+        "atom_weight": 9.012
+    },
+    {
+        "atomic_num": 5,
+        "chem_sym": "B",
+        "chem_name": "Boron",
+        "atom_weight": 10.811
+    },
+    {
+        "atomic_num": 6,
+        "chem_sym": "C",
+        "chem_name": "Carbon",
+        "atom_weight": 12.011
+    },
+    {
+        "atomic_num": 7,
+        "chem_sym": "N",
+        "chem_name": "Nitrogen",
+        "atom_weight": 14.007
+    },
+    {
+        "atomic_num": 8,
+        "chem_sym": "O",
+        "chem_name": "Oxygen",
+        "atom_weight": 15.999
+    },
+    {
+        "atomic_num": 9,
+        "chem_sym": "F",
+        "chem_name": "Fluorine",
+        "atom_weight": 18.998
+    },
+    {
+        "atomic_num": 10,
+        "chem_sym": "Ne",
+        "chem_name": "Neon",
+        "atom_weight": 20.180
+    },
+    {
+       "atomic_num": 11,
+        "chem_sym": "Na",
+        "chem_name": "Sodium",
+        "atom_weight": 22.990
+    },
+    {
+        "atomic_num": 12,
+        "chem_sym": "Mg",
+        "chem_name": "Magnesium",
+        "atom_weight": 24.305
+    },
+    {
+        "atomic_num": 13,
+        "chem_sym": "Al",
+        "chem_name": "Aluminium",
+        "atom_weight": 26.982
+    },
+    {
+        "atomic_num": 14,
+        "chem_sym": "Si",
+        "chem_name": "Silicon",
+        "atom_weight": 28.086
+    },
+    {
+        "atomic_num": 15,
+        "chem_sym": "P",
+        "chem_name": "Phosphorus",
+        "atom_weight": 30.974
+    },
+    {
+        "atomic_num": 16,
+        "chem_sym": "S",
+        "chem_name": "Sulfur",
+        "atom_weight": 32.066
+    },
+    {
+        "atomic_num": 17,
+        "chem_sym": "Cl",
+        "chem_name": "Chlorine",
+        "atom_weight": 35.453
+    },
+    {
+        "atomic_num": 18,
+        "chem_sym": "Ar",
+        "chem_name": "Argon",
+        "atom_weight": 39.948
+    }
+]
